d014c87d655df01f0798d619ee7c84274d949590
Groups <esokullu@gmail.com>
2018-12-11 05:45:47 -0800

    will not use params

--- index.php
+++ index.php
@@ -4,4 +4,4 @@ require "vendor/autoload.php";
 
 $templates = new League\Plates\Engine(__DIR__ . '/site/templates');
 
-echo $templates->render($_GET["page"], ['id' => $_GET["id"]]);
+echo $templates->render($_GET["page"], []);
