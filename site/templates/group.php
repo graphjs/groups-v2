4bb7482b89df29338c91d4e7f530890643ef6fd1
Groups <esokullu@gmail.com>
2018-12-14 05:07:14 -0800

    bugfixes

--- site/templates/group.php
+++ site/templates/group.php
@@ -6,6 +6,22 @@
     ]);
 ?>
 
-<main role="main" class="groups-content container-fluid p-0 mt-0">
-  <graphjs-group id="<?=$_GET["id"]?>" box="disabled"></graphjs-group>
+<main role="main" class="groups-content container-fluid p-0 mt-0"> 
+  <script>
+    function $_GET(param) {
+        var vars = {};
+        window.location.href.replace( location.hash, '' ).replace( 
+            /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+            function( m, key, value ) { // callback
+                vars[key] = value !== undefined ? value : '';
+            }
+        );
+
+        if ( param ) {
+            return vars[param] ? vars[param] : null;	
+        }
+        return vars;
+    }
+    document.writeln('<graphjs-group id="'.$_GET["id"].'" box="disabled"></graphjs-group>');
+  </script>  
 </main>
--- site/templates/profile.php
+++ site/templates/profile.php
@@ -7,5 +7,21 @@
 ?>
 
 <main role="main" class="groups-content container-fluid p-0 mt-0">
-  <graphjs-profile id="<?=$_GET["id"]?>" box="disabled"></graphjs-profile>
+  <script>
+    function $_GET(param) {
+        var vars = {};
+        window.location.href.replace( location.hash, '' ).replace( 
+            /[?&]+([^=&]+)=?([^&]*)?/gi, // regexp
+            function( m, key, value ) { // callback
+                vars[key] = value !== undefined ? value : '';
+            }
+        );
+
+        if ( param ) {
+            return vars[param] ? vars[param] : null;	
+        }
+        return vars;
+    }
+    document.writeln('<graphjs-profile id="'.$_GET["id"].'" box="disabled"></graphjs-profile>');
+  </script>
 </main>
