5d0e3a27591be2894f5bd447503ac6e0158d2657
Groups <esokullu@gmail.com>
2018-12-16 15:13:16 -0800

    added invite box

--- site/templates/home.php
+++ site/templates/home.php
@@ -20,10 +20,19 @@
       <graphjs-feed></graphjs-feed>
     </section>
     <aside class="d-none d-lg-block col-lg-4">
-      <graphjs-auth-register min-width="100%" max-width="100%"></graphjs-auth-register>
-      <div>
-
-      </div>
+        <div class="register-box d-none"> 
+             <graphjs-auth-register min-width="100%" max-width="100%"></graphjs-auth-register>
+        </div>
+      <div class="private-features d-none">
+            <a href="https://www.addtoany.com/share#url=https%3A%2F%2Fgr.ps&amp;title=" target="_blank"><img src="https://static.addtoany.com/buttons/a2a.svg" width="32" height="32" style="background-color:royalblue"></a>
+            <a href="https://www.addtoany.com/add_to/email?linkurl=https%3A%2F%2Fgr.ps&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/email.svg" width="32" height="32" style="background-color:royalblue"></a>
+            <a href="https://www.addtoany.com/add_to/facebook?linkurl=https%3A%2F%2Fgr.ps&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/facebook.svg" width="32" height="32" style="background-color:royalblue"></a>
+            <a href="https://www.addtoany.com/add_to/twitter?linkurl=https%3A%2F%2Fgr.ps&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/twitter.svg" width="32" height="32" style="background-color:royalblue"></a>
+            <a href="https://www.addtoany.com/add_to/linkedin?linkurl=https%3A%2F%2Fgr.ps&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/linkedin.svg" width="32" height="32" style="background-color:royalblue"></a>
+            <a href="https://www.addtoany.com/add_to/whatsapp?linkurl=https%3A%2F%2Fgr.ps&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/whatsapp.svg" width="32" height="32" style="background-color:royalblue"></a>
+            <a href="https://www.addtoany.com/add_to/reddit?linkurl=https%3A%2F%2Fgr.ps&amp;linkname=" target="_blank"><img src="https://static.addtoany.com/buttons/reddit.svg" width="32" height="32" style="background-color:royalblue"></a>
+            </div>
     </aside>
+
   </div>
 </main>
