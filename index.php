a60f7300689481e92c28408c6665295b6d065e10
Ozan İlbey Yılmaz <ozanilbey@ozanilbey.net>
2019-01-20 20:13:33 +0300

    Added Notifications module and Feed page & Fixed navigation bar structure

--- index.php
+++ index.php
@@ -6,18 +6,18 @@ $init = include(__DIR__ . "/lib/init.php");
 $templates = new League\Plates\Engine(__DIR__ . '/site/templates');
 
 $goal = "show";
-$public_id = '393CD160-2F38-4A7E-BF5A-FA0657FD649C';
+$public_id = '0AA5B31C-45FE-41F6-BF91-5CB12076468C';
 $primary_color = '#6f879f';
 $text_color = '#3f5f7f';
 $background_color = '#ffffff';
-$host = "https://gjd1602f384a7ebf5afa0657fd649c.herokuapp.com/";
+$host = "https://gjb31c45fe41f6bf915cb12076468c.herokuapp.com";
 $stream_host = "";
 $theme = "light";
 
 echo $templates->render($_GET["page"], [
         "goal"             => $goal,
-        "brand"            => "UX Lovers",
-        "public_id"             => $public_id, // $init($public_id), // (new \Tholu\Packer\Packer($init($public_id)))->pack()
+        "brand"            => "Sample Page",
+        "public_id"        => $public_id, // $init($public_id), // (new \Tholu\Packer\Packer($init($public_id)))->pack()
         "primaryColor"     => $primary_color,
         "textColor"        => $text_color,
         "backgroundColor"  => $background_color,
